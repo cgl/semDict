4938e9f568a7c4237aba57eefa4167c0dd1d76df
Cgl <cagilulusahin@gmail.com>
2016-10-05 20:45:26 +0300

    son

--- semDictBackend/semDictSite/settings.py
+++ semDictBackend/semDictSite/settings.py
@@ -75,9 +75,15 @@ WSGI_APPLICATION = 'semDict.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'sem',
+        'NAME': 'semDictDB',
+        'PASSWORD': 'qwopkl',
+        'HOST' : 127.0.0.1 ,
+#        'TEST': {
+#            'NAME': 'mytestdatabase',
+#        },
+    },
 }
 
 
